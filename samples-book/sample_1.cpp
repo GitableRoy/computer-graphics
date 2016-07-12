65a4d1181ce16c223c38693165f96fa5a8b79760
Leroy <roylee1920@hotmail.com>
2016-07-12 18:02:17 -0400

    first commit

---
+++ samples-book/sample_1.cpp
@@ -0,0 +1,37 @@
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void display(void) {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_POINTS);
+    glVertex2i(0, 0);
+    glVertex2i(0, 299);
+    glVertex2i(399, 0);
+    glVertex2i(399, 299);
+  glEnd();
+  glFlush();
+}
+
+int main(int argc, char** argv) {
+  /* code */
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(400, 300);
+  glutInitWindowPosition(200, 100);
+  glutCreateWindow("sample");
+  glClearColor(0.0, 0.0, 0.0);
+  gluOrtho2D(0.0, 400.0, 0.0, 300.0);
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
+}
