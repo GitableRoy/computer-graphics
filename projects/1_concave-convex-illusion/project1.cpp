2ab0cd3f1fd20ab0be2449c2317f67dd8b26415f
Leroy <roylee1920@hotmail.com>
2016-07-21 02:27:04 -0400

    Okay, now gradient is REALLY perfected.  Reversibility added to gradient. TODO -  make gradient a function

--- projects/1_concave-convex-illusion/project1.cpp
+++ projects/1_concave-convex-illusion/project1.cpp
@@ -28,7 +28,23 @@
 #define WIDTH  800
 #define HEIGHT 600
 
-#define RADIUS 200
+#define RADIUS 100
+
+GLfloat r(double angle) {
+    return sin(angle);
+}
+
+void createOval(void) {
+    glBegin(GL_LINES);
+
+    double t, p = 3.141593 / 180;
+    for(int i = 0; i < 360; i = i + 1) {
+        t = i * p;
+        glVertex2f(r(t) * cos(t), r(t) * sin(t));
+    }
+
+    glEnd();
+}
 
 void drawCircle (void) {
     /**
@@ -57,21 +73,20 @@ void drawCircle (void) {
 //        else x_position = sqrt( pow((y_position - y_center), 2) - pow(RADIUS, 2) ) + x_center;
 
         // Change Color
-        gradient_color = ((y_position + RADIUS )/ (2 * RADIUS));
+        gradient_color = fabs((y_position + RADIUS )/ (2 * RADIUS));
         glColor3f( gradient_color, gradient_color, gradient_color);
         std::cout << "Gradient Color is: " << gradient_color << std::endl;
 
         // Draw Line
         glVertex2f( (-1 * x_position), y_position );
         glVertex2f(x_position, y_position);
-        std::cout << "X: "<< x_position << ", Y: " << y_position << std::endl;
+//        std::cout << "X: "<< x_position << ", Y: " << y_position << std::endl;
     }
 
     glEnd();
 
     std::cout << "Circle Drawn" << std::endl;
 }
-
 
 void createCircle(int x_center, int y_center, int radius) {
     /**
@@ -93,27 +108,27 @@ void createCircle(int x_center, int y_center, int radius) {
 
     glBegin(GL_LINES);
 
-    for (float angle = 90; angle <= 270; angle += 0.1) {
+    for (float angle = 90; angle <= 270; angle += 0.05	) {
 
         std::cout << angle << std::endl;
 
-        // color is equal to percentage of angle completion
+        /* color is equal to percentage of angle completion */
         color = (angle - 90) / 180;
         glColor3f( color, color, color);
 
         std::cout << "Color is: " << color << std::endl;
 
-        // start of line at x,y points based on angle
+        /* start of line at x,y points based on angle */
         x_line_start = radius * cos(angle) + x_center;
         y_line_start = radius * sin(angle) + y_center;
 
-        // end of line at x´, y´ points based on angle
-        x_line_end = radius * cos(135 - angle) + x_center;
-        y_line_end = radius * sin(135 - angle) + y_center;
+        /* end of line at x´, y´ points based on angle  */
+        x_line_end = radius * cos(180 - angle) + x_center;
+        y_line_end = radius * sin(180 - angle) + y_center;  // unneccesary, the line is horizonal
 
-        // start line, end line
+        /* start line, end line */
         glVertex2f( x_line_start, y_line_start);
-        glVertex2f( x_line_end, y_line_start);  // y_line_end not neccesary because the circle is horizontal
+        glVertex2f( x_line_end, y_line_start);              // using line
 
     }
 
@@ -125,10 +140,13 @@ void createCircle(int x_center, int y_center, int radius) {
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.0, 0.0, 0.0);
-    createCircle(400, 300, RADIUS);
+//    createCircle(400, 300, RADIUS);
 //    createCircle(300, 200, RADIUS);
 //    createCircle(400, 300, RADIUS);
 //    createCircle(600, 500, RADIUS);
+//    createOval();
+
+    drawCircle();
 
     glFlush();
 }
@@ -157,8 +175,8 @@ int main(int argc, char** argv) {
 
     glClearColor(0.5, 0.5, 0.5, 0.0);
 
-    gluOrtho2D(0.0, WIDTH, 0.0, HEIGHT);
-//    gluOrtho2D(-2.0, 2.0, -1.5, 1.5);
+//    gluOrtho2D(0.0, WIDTH, 0.0, HEIGHT);
+    gluOrtho2D(-200.0, 200.0, -150.0, 150.0);
     glutDisplayFunc(display);
 //    glutReshapeFunc(reshape);
     glutMainLoop();
