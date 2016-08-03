7c3e06a2bb0c5b302536dde1b2c3ac595f65c157
Leroy <roylee1920@hotmail.com>
2016-08-03 12:40:02 -0400

    Turned initial polygon into a class

--- projects/3_interactive-inside-outside-test/project3.cpp
+++ projects/3_interactive-inside-outside-test/project3.cpp
@@ -16,24 +16,56 @@
 #endif
 
 int width = 400,
-height = 600,
-vert[100][2],
-n = 0,
-type = GL_LINE_STRIP,
-v;
+height = 600;
+
+class Polygon {
+public:
+    int vert[100][2],
+    numberOfVertices = 0,
+    type = GL_LINE_STRIP,
+    vertexIndex;
+
+    // public:
+    bool intersect(void) {
+        return false;
+    }
+
+};
+
+class Vertex {
+private:
+    int color[1][3],
+    xyLocation[1][2];
+
+public:
+    Vertex(int x, int y){
+        xyLocation[0][0] = x;
+        xyLocation[0][1] = y;
+    }
+
+    int getXYLocation() {
+        return 0;
+    }
+
+    void setColor() {}
+};
+
 
 bool rubberbanding=false,
 antialiasing=false;
 
+Polygon polly;
+
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
-    if (n==1 && (type==GL_LINE_STRIP || type == GL_LINE_LOOP)) {
+
+    if (polly.numberOfVertices==1 && (polly.type==GL_LINE_STRIP || polly.type == GL_LINE_LOOP)) {
         glBegin(GL_POINTS);
-        glVertex2iv(vert[0]);
+        glVertex2iv(polly.vert[0]);
         glEnd();
     }
-    glBegin(type);
-    for (int i = 0; i < n; i++) glVertex2iv(vert[i]);
+    glBegin(polly.type);
+    for (int i = 0; i < polly.numberOfVertices; i++) glVertex2iv(polly.vert[i]);
     glEnd();
     glutSwapBuffers();
 }
@@ -49,20 +81,22 @@ void keyboard(unsigned char key, int x, int y) {
                 glDisable(GL_LINE_SMOOTH);
             }
             break;
-        case 'r': n = 0; break;
-//        case 'l': type = GL_LINE_STRIP; break;
-        case 'c': type = GL_LINE_LOOP; break;
-        case 'v': type = GL_POINTS; break;
-//        case 'e': std::cout<< "Enter button working" << std::endl;
+        case 'r': polly.numberOfVertices = 0; break;
+            //        case 'l': polly.type = GL_LINE_STRIP; break;
+        case 'c': polly.type = GL_LINE_LOOP;
+            if (polly.vertexIndex < 5) std::cout<< "4 vetices button working" << std::endl;
+            break;
+        case 'v': polly.type = GL_POINTS; break;
+            //        case 'e': std::cout<< "Enter button working" << std::endl;
     }
     glutPostRedisplay();
 }
 
 int findVertex(int x, int y) {
     int dx, dy;
-    for(int i = 0; i < n; i++) {
-        dx = vert[i][0] - x;
-        dy = vert[i][1] - y;
+    for(int i = 0; i < polly.numberOfVertices; i++) {
+        dx = polly.vert[i][0] - x;
+        dy = polly.vert[i][1] - y;
         if(dx*dx + dy*dy < 16) return i;
     }
     return -1;
@@ -72,25 +106,25 @@ void mouse (int button, int state, int x, int y) {
     switch(button) {
         case GLUT_LEFT_BUTTON:
             if(state == GLUT_DOWN) {
-                v = n++;
-                vert[v][0] = x;
-                vert[v][1] = height - 1 - y;
+                polly.vertexIndex = polly.numberOfVertices++;
+                polly.vert[polly.vertexIndex][0] = x;
+                polly.vert[polly.vertexIndex][1] = height - 1 - y;
                 rubberbanding = true;
                 glutPostRedisplay();
             }
             else rubberbanding = false;
             break;
         case GLUT_RIGHT_BUTTON:
-            if((state==GLUT_DOWN) && (v = findVertex(x, height-1-y)) != -1){
+            if((state==GLUT_DOWN) && (polly.vertexIndex = findVertex(x, height-1-y)) != -1){
                 if(glutGetModifiers () == GLUT_ACTIVE_CTRL) {
-                    for (int i=v; i<n-1; i++) {
-                        vert[i][0]= vert[i+1][0];
-                        vert[i][1]= vert[i+1][1];
+                    for (int i=polly.vertexIndex; i<polly.numberOfVertices-1; i++) {
+                        polly.vert[i][0]= polly.vert[i+1][0];
+                        polly.vert[i][1]= polly.vert[i+1][1];
                     }
-                    n--;
+                    polly.numberOfVertices--;
                 } else {
-                    vert[v][0]=x;
-                    vert[v][1]=height-1-y;
+                    polly.vert[polly.vertexIndex][0]=x;
+                    polly.vert[polly.vertexIndex][1]=height-1-y;
                     rubberbanding = true;
                 }
                 glutPostRedisplay();
@@ -102,8 +136,8 @@ void mouse (int button, int state, int x, int y) {
 
 void motion(int x, int y) {
     if(rubberbanding) {
-        vert[v][0] = x;
-        vert[v][1] = height -1 -y;
+        polly.vert[polly.vertexIndex][0] = x;
+        polly.vert[polly.vertexIndex][1] = height -1 -y;
         glutPostRedisplay();
     }
 }
@@ -115,21 +149,21 @@ int main(int argc, char** argv) {
     glutInitWindowSize(width, height);
     glutInitWindowPosition(50, 100);
     glutCreateWindow("Project 3");
-    
-    
+
+
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glColor3f(0,0,0);
-    
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0, width, 0, height);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
+
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
     glutMotionFunc(motion);
-    
+
     glutMainLoop();
     return 0;
 }
